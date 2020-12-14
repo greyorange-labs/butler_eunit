95355277b753bdf05680f7f6fe1282c765a70fae
Hritik Soni <hritik.s@greyorange.com>
2020-12-14 08:59:54 +0000

    adding teardown provider

--- src/butler_eunit.erl
+++ src/butler_eunit.erl
@@ -5,4 +5,5 @@
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     {ok, State1} = butler_eunit_prv:init(State),
-    {ok, State1}.
+    {ok, State2} = butler_eunit_tear_prv:init(State1),
+    {ok, State2}.
--- src/butler_eunit_tear_prv.erl
+++ src/butler_eunit_tear_prv.erl
@@ -2,7 +2,7 @@
 
 -export([init/1, do/1, format_error/1]).
 
--define(PROVIDER, eunit).
+-define(PROVIDER, eunit_tear).
 -define(NAMESPACE, butler_eunit).
 -define(DEPS, [app_discovery]).
 
@@ -19,20 +19,17 @@ init(State) ->
             {deps, ?DEPS},                % The list of dependencies
             {example, "rebar3 butler_eunit"}, % How to use the plugin
             {opts, []},                   % list of options understood by the plugin
-            {short_desc, "eunit setup for butler server"},
-            {desc, "eunit setup for butler server"}
+            {short_desc, "eunit teardown for butler server"},
+            {desc, "eunit teardown for butler server"}
     ]),
     {ok, rebar_state:add_provider(State, Provider)}.
 
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    os:cmd("rm -rf Mnesia.nonode@nohost"),
-    application:ensure_all_started(gproc),
-    metric_utils:init_metrics(),
-    butler_setup:initialize_all_caches(),
-    order_fulfilment_sup:initialize_simple_caches(),
-    butler_setup:init_database_from_model_list(models:all()),
+    application:stop(mnesia),
+    application:stop(boss_db),
+    application:stop(gproc),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
