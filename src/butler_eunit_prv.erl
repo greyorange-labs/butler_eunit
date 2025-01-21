89c7e073c5716283d3844b9daa57b8c037eb7f04
Amar Chand <amar.c@greyorange.com>
2025-01-21 18:09:40 +0530

    Update butler_eunit_prv.erl

--- src/butler_eunit_prv.erl
+++ src/butler_eunit_prv.erl
@@ -27,6 +27,7 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
+    io:format("Eunit: Pre setup [start]......~n"),
     os:cmd("rm -rf Mnesia.butler_server.test"),
     application:set_env(mnesia, dir, "Mnesia.butler_server.test"),
     application:ensure_all_started(gproc),
@@ -34,23 +35,26 @@ do(State) ->
     butler_setup:initialize_all_caches(),
     order_fulfilment_sup:initialize_simple_caches(),
     %% 1. Runs old migrations
+    io:format("Running Base migrations..........~n"),
     db_setup:init_databases([]),
     Apps = application:get_env(butler_server, x_runtime_apps, [gmc, non_gmc]),
     application:set_env(mnesia_migrate, verbose, false),
     application:set_env(erl_migrate, verbose, false),
     %% 2. Runs `GMC` migrations
+    io:format("Running GMC migrations..........~n"),
     case lists:member(gmc, Apps) of
         true -> ok = gmc_db_setup:init_migrations();
         false -> ok
     end,
     %% 3. Runs `GMR` migrations
+    io:format("Running GMR migrations..........~n"),
     case lists:member(non_gmc, Apps) of
         true -> ok = gmr_db_setup:init_migrations();
         false -> ok
     end,
     bsh_global_data:ensure_advance_logging_record(),
     bsh_sysmon:init_cache(),
-    io:format("Eunit: Pre setup done......~n"),
+    io:format("Eunit: Pre setup [complete]......~n"),,
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
